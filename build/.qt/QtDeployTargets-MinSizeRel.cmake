9bb3c1d22b20fa7373278d074dba234f00055af2
Dominique YOLIN <dominique@arcreane.com>
2025-11-19 16:30:12 +0100

    Delete build directory

--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Euronext_Surveillance_FILE C:/Users/aguil/OneDrive/Documents/GitHub/Euronext_Surveillance/build/MinSizeRel/Euronext_Surveillance.exe)
-set(__QT_DEPLOY_TARGET_Euronext_Surveillance_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Euronext_Surveillance_RUNTIME_DLLS C:/Qt/6.10.0/msvc2022_64/bin/Qt6Charts.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6OpenGL.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Core.dll)
