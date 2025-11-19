eeab619f8020890224bc08bacb82395858e0f1b8
Dominique YOLIN <dominique@arcreane.com>
2025-11-19 16:30:26 +0100

    Delete out directory

--- out/build/debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Euronext_Surveillance_FILE C:/Users/aguil/OneDrive/Documents/GitHub/Euronext_Surveillance/out/build/debug/Euronext_Surveillance.exe)
-set(__QT_DEPLOY_TARGET_Euronext_Surveillance_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Euronext_Surveillance_RUNTIME_DLLS C:/Qt/6.10.0/msvc2022_64/bin/Qt6Chartsd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6OpenGLWidgetsd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6OpenGLd.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Cored.dll)
